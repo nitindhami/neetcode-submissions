ca7ead8d50126caab112c9340e399b57d009e16c
Nitin Dhami <dhami.nitin@gmail.com>
2026-09-24 14:28:54 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaxProfit(int[] prices) 
+    {
+     int minPrice = prices[0];
+     int maxProfit = 0;
+
+    for (int i = 1; i < prices.Length; i++)
+    {
+        minPrice = Math.Min(minPrice, prices[i]);
+
+        int profit = prices[i] - minPrice;
+
+        maxProfit = Math.Max(maxProfit, profit);
+    }
+
+    return maxProfit;    
+    }
+}
